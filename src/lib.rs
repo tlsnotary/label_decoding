d45e409aa53749d33ee60df79f01b626ac93732a
themighty1 <you@example.com>
2022-08-24 15:49:06 +0300

    typestates work

--- src/lib.rs
+++ src/lib.rs
@@ -42,7 +42,7 @@ mod tests {
 
     use super::*;
     use num::{BigUint, FromPrimitive};
-    use prover::Prover;
+    use prover::{Prover, Prover2};
     use rand::{thread_rng, Rng, RngCore};
     use verifier::Verifier;
 
@@ -113,6 +113,75 @@ mod tests {
 
         // Hash commitment to the label_sum is sent to the Notary
         let label_sum_hashes = prover
+            .labelsum_commitment(&cipheretexts, &prover_labels)
+            .unwrap();
+
+        // Notary sends zero_sum and all deltas
+        let (deltas, zero_sums) = verifier.receive_labelsum_hash(label_sum_hashes);
+
+        // Prover generates the proof
+        let proofs = prover.create_zk_proof(zero_sums, deltas).unwrap();
+
+        // Verifier verifies the proof
+        assert_eq!(verifier.verify_many(proofs).unwrap(), true);
+    }
+
+    #[test]
+    fn e2e_test2() {
+        let prime = String::from(BN254_PRIME).parse::<BigUint>().unwrap();
+        let mut rng = thread_rng();
+
+        // OneTimeSetup is a no-op if the setup has been run before
+        let mut ots = OneTimeSetup::new();
+        ots.setup().unwrap();
+
+        // Our Poseidon is 16-width, so one permutation processes:
+        // 16 * 253 - 128 bits (salt) == 490 bytes. This is the size of the chunk.
+
+        // generate random plaintext of random size in range (0, 2000)
+        let mut plaintext = vec![0u8; rng.gen_range(0..2000)];
+        rng.fill_bytes(&mut plaintext);
+
+        // Normally, the Prover is expected to obtain her binary labels by
+        // evaluating the garbled circuit.
+        // To keep this test simple, we don't evaluate the gc, but we generate
+        // all labels of the Verifier and give the Prover her active labels.
+        let bit_size = plaintext.len() * 8;
+        let mut all_binary_labels: Vec<[u128; 2]> = Vec::with_capacity(bit_size);
+        let mut delta: u128 = rng.gen();
+        // set the last bit
+        delta |= 1;
+        for _ in 0..bit_size {
+            let label_zero: u128 = rng.gen();
+            all_binary_labels.push([label_zero, label_zero ^ delta]);
+        }
+        let prover_labels = choose(&all_binary_labels, &u8vec_to_boolvec(&plaintext));
+
+        let mut verifier = VerifierNode::new(true);
+        // passing proving key to the Prover (if he needs one)
+        let proving_key = verifier.get_proving_key().unwrap();
+        // produce ciphertexts which are sent to Prover for decryption
+        verifier.setup(&all_binary_labels);
+
+        let prover = Prover2::new(
+            proving_key,
+            prime,
+            plaintext,
+            Box::new(prover::PoseidonHasher {}),
+            Box::new(prover::ZkProver {}),
+        );
+
+        // Perform setup
+        let prover = prover.setup().unwrap();
+
+        // Commitment to the plaintext is sent to the Verifier
+        let (plaintext_hash, prover) = prover.plaintext_commitment().unwrap();
+
+        // Verifier sends back encrypted arithm. labels.
+        let cipheretexts = verifier.receive_pt_hashes(plaintext_hash);
+
+        // Hash commitment to the label_sum is sent to the Notary
+        let (label_sum_hashes, prover) = prover
             .labelsum_commitment(&cipheretexts, &prover_labels)
             .unwrap();
 
--- src/prover.rs
+++ src/prover.rs
@@ -4,8 +4,428 @@ use derive_macro::{define_accessors_trait, ProverDataGetSet};
 use json::{object, stringify_pretty};
 use num::{BigUint, FromPrimitive};
 use rand::{thread_rng, Rng};
+use std::env::temp_dir;
+use std::fs;
+use std::marker::PhantomData;
+use std::process::{Command, Output};
+use uuid::Uuid;
 
 use super::{PERMUTATION_COUNT, POSEIDON_WIDTH};
+
+pub trait State {}
+
+pub struct Setup {
+    proving_key: Vec<u8>,
+    field_prime: BigUint,
+    plaintext: Vec<u8>,
+}
+
+pub struct PlaintextCommitment {
+    proving_key: Vec<u8>,
+    field_prime: BigUint,
+    plaintext: Vec<u8>,
+    useful_bits: usize,
+    chunks: Vec<Vec<BigUint>>,
+    salts: Vec<BigUint>,
+    chunk_size: usize,
+}
+
+pub struct LabelsumCommitment {
+    proving_key: Vec<u8>,
+    field_prime: BigUint,
+    plaintext: Vec<u8>,
+    useful_bits: usize,
+    chunks: Vec<Vec<BigUint>>,
+    salts: Vec<BigUint>,
+    chunk_size: usize,
+    hashes_of_chunks: Vec<BigUint>,
+}
+
+pub struct ProofCreation {
+    plaintext: Vec<u8>,
+    useful_bits: usize,
+    chunks: Vec<Vec<BigUint>>,
+    chunk_size: usize,
+    labelsum_hashes: Vec<BigUint>,
+    hashes_of_chunks: Vec<BigUint>,
+}
+
+impl State for Setup {}
+impl State for PlaintextCommitment {}
+impl State for LabelsumCommitment {}
+impl State for ProofCreation {}
+
+pub struct PoseidonHasher {}
+pub struct ZkProver {}
+
+pub trait Hash {
+    fn hash(&self, bytes: &Vec<BigUint>) -> Result<BigUint, ProverError>;
+}
+
+impl Hash for PoseidonHasher {
+    /// Hashes inputs with the Poseidon hash. Inputs are field elements (FEs). The
+    /// amount of FEs must be POSEIDON_WIDTH * PERMUTATION_COUNT
+    fn hash(&self, inputs: &Vec<BigUint>) -> Result<BigUint, ProverError> {
+        // convert field elements into escaped strings
+        let strchunks: Vec<String> = inputs
+            .iter()
+            .map(|fe| String::from("\"") + &fe.to_string() + &String::from("\""))
+            .collect();
+        // convert to JSON array
+        let json = String::from("[") + &strchunks.join(", ") + &String::from("]");
+
+        let output = Command::new("node").args(["poseidon.mjs", &json]).output();
+        if output.is_err() {
+            return Err(ProverError::ErrorInPoseidonImplementation);
+        }
+        let output = output.unwrap();
+        // drop the trailing new line
+        println!("stderr was {:?}", String::from_utf8(output.stderr.to_vec()));
+        println!("stdout was {:?}", String::from_utf8(output.stdout.to_vec()));
+
+        let output = &output.stdout[0..output.stdout.len() - 1];
+        let str = String::from_utf8(output.to_vec());
+        if str.is_err() {
+            return Err(ProverError::ErrorInPoseidonImplementation);
+        }
+        let bi = str.unwrap().parse::<BigUint>();
+        if bi.is_err() {
+            return Err(ProverError::ErrorInPoseidonImplementation);
+        }
+        Ok(bi.unwrap())
+    }
+}
+
+pub trait ProveTrait {
+    fn prove(&self, input: &String) -> Result<Vec<u8>, ProverError>;
+}
+
+impl ProveTrait for ZkProver {
+    /// Produces a groth16 proof with snarkjs. Input must be a JSON string in the
+    /// "input.json" format which snarkjs expects.
+    fn prove(&self, input: &String) -> Result<Vec<u8>, ProverError> {
+        let mut path1 = temp_dir();
+        let mut path2 = temp_dir();
+        path1.push(format!("input.json.{}", Uuid::new_v4()));
+        path2.push(format!("proof.json.{}", Uuid::new_v4()));
+
+        fs::write(path1.clone(), input).expect("Unable to write file");
+        let output = Command::new("node")
+            .args([
+                "prove.mjs",
+                path1.to_str().unwrap(),
+                path2.to_str().unwrap(),
+            ])
+            .output();
+        fs::remove_file(path1).expect("Unable to remove file");
+        check_output(output)?;
+        let proof = fs::read(path2.clone()).unwrap();
+        fs::remove_file(path2).expect("Unable to remove file");
+        Ok(proof)
+    }
+}
+
+fn check_output(output: Result<Output, std::io::Error>) -> Result<(), ProverError> {
+    if output.is_err() {
+        return Err(ProverError::SnarkjsError);
+    }
+    if !output.unwrap().status.success() {
+        return Err(ProverError::SnarkjsError);
+    }
+    Ok(())
+}
+pub struct Prover2<S = Setup>
+where
+    S: State,
+{
+    poseidon: Box<dyn Hash>,
+    prover: Box<dyn ProveTrait>,
+    state: S,
+}
+
+impl Prover2 {
+    pub fn new(
+        proving_key: Vec<u8>,
+        field_prime: BigUint,
+        plaintext: Vec<u8>,
+        poseidon: Box<dyn Hash>,
+        prover: Box<dyn ProveTrait>,
+    ) -> Prover2<Setup> {
+        Prover2 {
+            state: Setup {
+                proving_key,
+                field_prime,
+                plaintext,
+            },
+            poseidon,
+            prover,
+        }
+    }
+}
+
+impl Prover2<Setup> {
+    // Performs setup. Splits plaintext into chunks and computes a hash of each
+    // chunk.
+    pub fn setup(self) -> Result<Prover2<PlaintextCommitment>, ProverError> {
+        if self.state.field_prime.bits() < 129 {
+            // last field element must be large enough to contain the 128-bit
+            // salt. In the future, if we need to support fields < 129 bits,
+            // we can put the salt into multiple field elements.
+            return Err(ProverError::WrongPrime);
+        }
+        let useful_bits = self.calculate_useful_bits(&self.state.field_prime);
+        let (chunk_size, chunks, salts) =
+            self.plaintext_to_chunks(useful_bits, &self.state.plaintext);
+
+        Ok(Prover2 {
+            state: PlaintextCommitment {
+                proving_key: self.state.proving_key,
+                field_prime: self.state.field_prime,
+                useful_bits,
+                plaintext: self.state.plaintext,
+                chunks,
+                salts,
+                chunk_size,
+            },
+            poseidon: self.poseidon,
+            prover: self.prover,
+        })
+    }
+
+    /// Calculates how many bits of plaintext we will pack into one field element.
+    /// Essentially, this is field_prime bit length minus 1.
+    fn calculate_useful_bits(&self, field_prime: &BigUint) -> usize {
+        (field_prime.bits() - 1) as usize
+    }
+
+    /// Create chunks of plaintext where each chunk consists of 16 field elements.
+    /// The last element's last 128 bits are reserved for the salt of the hash.
+    /// If there is not enough plaintext to fill the whole chunk, we fill the gap
+    /// with zero bits.
+    /// Returns (the size of a chunk (sans the salt), chunks, salts for each chunk)
+    fn plaintext_to_chunks(
+        &self,
+        useful_bits: usize,
+        plaintext: &Vec<u8>,
+    ) -> (usize, Vec<Vec<BigUint>>, Vec<BigUint>) {
+        // the amount of field elements in each chunk
+        let fes_per_chunk = POSEIDON_WIDTH * PERMUTATION_COUNT;
+        // the size of a chunk of plaintext not counting the salt
+        let chunk_size = useful_bits * fes_per_chunk - 128;
+        // plaintext converted into bits
+        let mut bits = u8vec_to_boolvec(&plaintext);
+        // chunk count (rounded up)
+        let chunk_count = (bits.len() + (chunk_size - 1)) / chunk_size;
+
+        // extend bits with zeroes to fill the last chunk
+        bits.extend(vec![false; chunk_count * chunk_size - bits.len()]);
+        let mut chunks: Vec<Vec<BigUint>> = Vec::with_capacity(chunk_count);
+        let mut salts: Vec<BigUint> = Vec::with_capacity(chunk_count);
+
+        let mut rng = thread_rng();
+        for chunk_of_bits in bits.chunks(chunk_size) {
+            let mut chunk: Vec<BigUint> = Vec::with_capacity(fes_per_chunk);
+
+            // split a chunk into field elements
+            for (i, fe_bits) in chunk_of_bits.chunks(useful_bits).enumerate() {
+                if i < fes_per_chunk - 1 {
+                    chunk.push(BigUint::from_bytes_be(&boolvec_to_u8vec(&fe_bits)));
+                } else {
+                    // last field element's last 128 bits are for the salt
+                    let salt = rng.gen::<[u8; 16]>();
+                    salts.push(BigUint::from_bytes_be(&salt));
+                    let mut bits_and_salt = fe_bits.to_vec();
+                    bits_and_salt.extend(u8vec_to_boolvec(&salt).iter());
+                    chunk.push(BigUint::from_bytes_be(&boolvec_to_u8vec(&bits_and_salt)));
+                };
+            }
+            chunks.push(chunk);
+        }
+        (chunk_size, chunks, salts)
+    }
+}
+
+impl Prover2<PlaintextCommitment> {
+    pub fn plaintext_commitment(
+        self,
+    ) -> Result<(Vec<BigUint>, Prover2<LabelsumCommitment>), ProverError> {
+        let hashes = self.hash_chunks(&self.state.chunks)?;
+
+        Ok((
+            hashes.clone(),
+            Prover2 {
+                state: LabelsumCommitment {
+                    proving_key: self.state.proving_key,
+                    field_prime: self.state.field_prime,
+                    useful_bits: self.state.useful_bits,
+                    plaintext: self.state.plaintext,
+                    hashes_of_chunks: hashes,
+                    chunks: self.state.chunks,
+                    salts: self.state.salts,
+                    chunk_size: self.state.chunk_size,
+                },
+                poseidon: self.poseidon,
+                prover: self.prover,
+            },
+        ))
+    }
+
+    /// Hashes each chunk with Poseidon and returns digests for each chunk.
+    fn hash_chunks(&self, chunks: &Vec<Vec<BigUint>>) -> Result<Vec<BigUint>, ProverError> {
+        chunks
+            .iter()
+            .map(|chunk| {
+                if chunk.len() != POSEIDON_WIDTH * PERMUTATION_COUNT {
+                    return Err(ProverError::WrongPoseidonInput);
+                }
+                Ok(self.poseidon.hash(chunk)?)
+            })
+            .collect()
+    }
+}
+
+impl Prover2<LabelsumCommitment> {
+    // Computes the sum of all arithmetic labels for each chunk of plaintext.
+    /// Returns the hash of each sum.
+    pub fn labelsum_commitment(
+        self,
+        ciphertexts: &Vec<[Vec<u8>; 2]>,
+        labels: &Vec<u128>,
+    ) -> Result<(Vec<BigUint>, Prover2<ProofCreation>), ProverError> {
+        let sums = self.compute_label_sums(ciphertexts, labels)?;
+
+        let res: Result<Vec<BigUint>, ProverError> = sums
+            .iter()
+            .map(|sum| Ok(self.poseidon.hash(&vec![sum.clone()])?))
+            .collect();
+        if res.is_err() {
+            return Err(ProverError::ErrorInPoseidonImplementation);
+        }
+        let labelsum_hashes = res.unwrap();
+
+        Ok((
+            labelsum_hashes.clone(),
+            Prover2 {
+                state: ProofCreation {
+                    useful_bits: self.state.useful_bits,
+                    plaintext: self.state.plaintext,
+                    chunks: self.state.chunks,
+                    chunk_size: self.state.chunk_size,
+                    labelsum_hashes,
+                    hashes_of_chunks: self.state.hashes_of_chunks,
+                },
+                poseidon: self.poseidon,
+                prover: self.prover,
+            },
+        ))
+    }
+
+    /// Decrypts each encrypted arithm. label based on the point-and-permute
+    /// (p&p) bit of our corresponding active binary label. Computes the sum of
+    /// all arithmetic labels for each chunk of plaintext. Returns the sums.
+    fn compute_label_sums(
+        &self,
+        ciphertexts: &Vec<[Vec<u8>; 2]>,
+        labels: &Vec<u128>,
+    ) -> Result<Vec<BigUint>, ProverError> {
+        if ciphertexts.len() != labels.len() {
+            return Err(ProverError::IncorrectEncryptedLabelSize);
+        }
+        if self.state.plaintext.len() * 8 != ciphertexts.len() {
+            return Err(ProverError::IncorrectEncryptedLabelSize);
+        }
+
+        // Each chunk is hashed separately
+        let mut label_sum_hashes: Vec<BigUint> = Vec::with_capacity(self.state.chunks.len());
+
+        let ct_iter = ciphertexts.chunks(self.state.chunk_size);
+        let lb_iter = labels.chunks(self.state.chunk_size);
+        // process a pair (chunk of ciphertexts, chunk of corresponding labels) at a time
+        for (chunk_ct, chunk_lb) in ct_iter.zip(lb_iter) {
+            // accumulate the label sum for one chunk here
+            let mut label_sum = BigUint::from_u8(0).unwrap();
+            for (ct_pair, label) in chunk_ct.iter().zip(chunk_lb) {
+                let key = Aes128::new_from_slice(&label.to_be_bytes()).unwrap();
+                // if binary label's p&p bit is 0, decrypt the 1st ciphertext,
+                // otherwise decrypt the 2nd one.
+                let mut ct = [0u8; 16];
+                if label & 1 == 0 {
+                    ct.copy_from_slice(&ct_pair[0]);
+                } else {
+                    ct.copy_from_slice(&ct_pair[1]);
+                }
+                let mut ct: GenericArray<u8, U16> = GenericArray::from(ct);
+                key.decrypt_block(&mut ct);
+                // add the decrypted arithmetic label to the sum
+                label_sum += BigUint::from_bytes_be(&ct);
+            }
+
+            label_sum_hashes.push(label_sum);
+        }
+        Ok(label_sum_hashes)
+    }
+}
+
+impl Prover2<ProofCreation> {
+    // Creates a zk proof of label decoding for each chunk of plaintext. Returns
+    // proofs in the snarkjs's "proof.json" format
+    pub fn create_zk_proof(
+        self,
+        zero_sum: Vec<BigUint>,
+        deltas: Vec<BigUint>,
+    ) -> Result<Vec<Vec<u8>>, ProverError> {
+        let inputs = self.create_proof_inputs(zero_sum, deltas);
+        let mut proofs = Vec::with_capacity(inputs.len());
+        for input in inputs {
+            proofs.push(self.prover.prove(&input)?);
+        }
+        Ok(proofs)
+    }
+
+    // Creates inputs in the "input.json" format
+    fn create_proof_inputs(&self, zero_sum: Vec<BigUint>, mut deltas: Vec<BigUint>) -> Vec<String> {
+        // Since the last chunk is padded with zero plaintext, we also zero-pad
+        // the corresponding deltas of the last chunk.
+        let delta_pad_count = self.state.chunk_size * self.state.chunks.len() - deltas.len();
+        deltas.extend(vec![BigUint::from_u8(0).unwrap(); delta_pad_count]);
+
+        // we will have as many proofs as there are chunks of plaintext
+        let mut inputs: Vec<String> = Vec::with_capacity(self.state.chunks.len());
+        let chunks_of_deltas: Vec<&[BigUint]> = deltas.chunks(self.state.chunk_size).collect();
+
+        for count in 0..self.state.chunks.len() {
+            // convert each field element of plaintext into a string
+            let plaintext: Vec<String> = self.state.chunks[count]
+                .iter()
+                .map(|bigint| bigint.to_string())
+                .collect();
+
+            // convert all deltas to strings
+            let deltas_str: Vec<String> = chunks_of_deltas[count]
+                .iter()
+                .map(|v| v.to_string())
+                .collect();
+
+            // split deltas into groups corresponding to the field elements
+            // of our Poseidon circuit
+            let deltas_fes: Vec<&[String]> = deltas_str.chunks(self.state.useful_bits).collect();
+
+            // prepare input.json
+            let input = object! {
+                plaintext_hash: self.state.hashes_of_chunks[count].to_string(),
+                label_sum_hash: self.state.labelsum_hashes[count].to_string(),
+                sum_of_zero_labels: zero_sum[count].to_string(),
+                plaintext: plaintext,
+                delta: deltas_fes[0..deltas_fes.len()-1],
+                // last field element's deltas form a separate input
+                delta_last: deltas_fes[deltas_fes.len()-1]
+            };
+            let s = stringify_pretty(input, 4);
+            inputs.push(s);
+        }
+        inputs
+    }
+}
 
 #[derive(Debug)]
 pub enum ProverError {
